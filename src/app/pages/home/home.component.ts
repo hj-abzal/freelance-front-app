beef579934a356a6a56aa5642c9afb6e460f3e8b
Abzal <abzal008suan@gmail.com>
2022-07-08 16:53:45 +0600

    add test changes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  text: string = '';
+  count: number = 0;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private http: HttpClient
+  ) {
   }
 
+  ngOnInit(): void {
+    this.http.get('http://localhost:3000').subscribe((res: any) => {
+      this.text = res.name;
+    })
+  }
+
+  inc() {
+    this.count += 1;
+  }
+
+  dec() {
+    this.count -= 1;
+  }
 }
